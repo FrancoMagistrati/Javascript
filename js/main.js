cd0ef66240d90ffd98d779a8a11f95b1271eb05f
Franco <francomagistrati@gmail.com>
2023-03-30 23:12:50 -0300

    PreEntrega Magistrati

---
+++ js/main.js
@@ -0,0 +1,70 @@
+let continuar = true
+let nombre = "Alumnos"
+let profesor = "Franco Magistrati"
+let nota1
+let nota2
+let resultado
+function login (){
+	let ingresar = prompt("Ingresa el nombre del profesor")
+	let entrar = true
+	if(ingresar === "Franco Magistrati")
+	{
+		alert("Bienvenido " + profesor)
+
+
+	}else{
+		alert("Profesor incorrecto")
+		entrar = false
+		
+	}
+return entrar
+
+}
+
+if(login()){
+	
+	do{
+		let nuevonombre = prompt("Nuevo nombre")
+		
+	if(nuevonombre != null && nuevonombre !=""){
+		 nota1 = parseInt(prompt("Nota del primer parcial"))
+		if(nota1>0 && nota1 <=10){
+		
+			nota2 = parseInt(prompt("Nota del segundo parcial"))
+				if(nota2 >0 && nota2<=10){
+					nombre = nombre + "\n" + nuevonombre + " Primer parcial " + nota1 + " Segundo parical " + nota2 
+			
+					alert(nombre)
+					resultado = (nota1 + nota2) /2
+					if(resultado>=6)
+					{
+						let aprobado = nuevonombre + " Esta aprobado su promedio es de: " + resultado
+						alert(aprobado)
+					}else{
+						let desaprobado = nuevonombre + " Esta desaprobado su promedio es de: " + resultado
+						alert(desaprobado)
+					}
+				}else {alert("Ingresaste mal la segunda nota")}
+
+			}else{alert("Ingresaste mal la nota")}
+	
+		
+		
+	
+		}else{
+		let pregunta = prompt("Desea agregar nombre? \n-Si \n-No")
+		if(pregunta == "si"){
+		nuevonombre
+	}else {
+		
+	alert("Hasta luego!!!")
+	
+	continuar = false
+	}
+	
+	}
+	}while(continuar)
+
+
+}
+
